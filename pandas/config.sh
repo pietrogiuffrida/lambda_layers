f80da560ffa0c58321d47fae20c216dd01b97bd5
Pietro Giuffrida <p.giuffrida@crif.com>
2020-03-21 11:36:22 +0000

    New config.sh, the description now contains also package list

--- pandas/config.sh
+++ pandas/config.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 LAYER_NAME='layer_pandas_0_25_3.zip'
-DESCRIPTION="pandas 0.25.3 for python 3.7.6"
 
 # A LIST OF PACKAGE TO INSTALL, SPACE SEPARATED
-PACKAGES="pandas==0.25.3"
+PACKAGES="pandas==0.25.3 python-dateutil pytz"
 
 RUNTIMES="python3.7"
+
+DESCRIPTION="pandas 0.25.3 for python 3.7.6. Contains: $PACKAGES"
--- scikit-learn/config.sh
+++ scikit-learn/config.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 LAYER_NAME='layer_scikit-learn_0_22_1.zip'
-DESCRIPTION="scikit-learn 0.22.1 for python 3.7.6"
 
 # A LIST OF PACKAGE TO INSTALL, SPACE SEPARATED
 PACKAGES="scikit-learn==0.22.1"
 
 RUNTIMES="python3.7"
+
+DESCRIPTION="scikit-learn 0.22.1 for python 3.7.6. Contains: $PACKAGES"
