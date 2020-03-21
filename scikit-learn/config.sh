cbc7637e90d091d530893bb4e0076d84ec9d18e6
Pietro Giuffrida <p.giuffrida@crif.com>
2020-03-21 12:17:35 +0000

    Adding dependencies for scikit

--- scikit-learn/config.sh
+++ scikit-learn/config.sh
@@ -3,7 +3,7 @@
 LAYER_NAME='layer_scikit-learn_0_22_1.zip'
 
 # A LIST OF PACKAGE TO INSTALL, SPACE SEPARATED
-PACKAGES="scikit-learn==0.22.1"
+PACKAGES="scikit-learn==0.22.1 joblib==0.14.1"
 
 RUNTIMES="python3.7"
 
